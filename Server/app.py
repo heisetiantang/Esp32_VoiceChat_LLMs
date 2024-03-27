857d3480d0033f9a8634c05494c4f3363bfb6cb8
heisetiantang <92718554+heisetiantang@users.noreply.github.com>
2024-03-27 17:49:14 +0800

    app.py注释

--- Server/app.py
+++ Server/app.py
@@ -17,7 +17,7 @@ APISecret = ""  # 填写控制台中获取的 APISecret 信息
 APIKey = ""  # 填写控制台中获取的 APIKey 信息
 
 # 用于配置大模型版本，默认“general/generalv2”
-domain = "generalv3"   # v1.5版本
+domain = "generalv3"   # v3版本
 # domain = "generalv2"    # v2.0版本
 # 云端环境的服务地址
 Spark_url = "ws://spark-api.xf-yun.com/v3.1/chat"  # v1.5环境的地址
